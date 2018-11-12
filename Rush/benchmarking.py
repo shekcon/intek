427ba5640c78fd464bf312643b4151f5742d316b
Q.Sang <quangsang9773@gmail.com>
2018-11-13 01:02:45 +0700

    Update code for bechmarking from Huong Nam :D

--- Rush/benchmarking.py
+++ Rush/benchmarking.py
@@ -1,70 +1,73 @@
 #!/usr/bin/env python3
-# set interpreter
-from argparse import ArgumentParser
-import sys
+import argparse
 import subprocess
 import resource
-import tracemalloc
-import os
 import cProfile
-import importlib
 import pstats
 
 
-def read_resource(usage):
-    RESOURCES = [('ru_utime', 'User time'),
-                 ('ru_stime', 'System time'),
-                 ('ru_maxrss', 'Usage memory')
-                 ]
-    usage = resource.getrusage(usage)
-    for name, desc in RESOURCES:
-        print('{:<15} = {}'.format(
-            desc, str(getattr(usage, name))
-            + " s" if name != 'ru_maxrss' else str(getattr(usage,
-                                                           name) // 1024)
-            + " KB"))
+# import pstats
+
+def memory_allocation():
+    '''
+    ru_maxrss: maximum resident set size
+    '''
+    memory_usage = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
+    print('Memory usage: ' + str(memory_usage) + ' KB')
 
 
-def display_trace():
-    snapshot = tracemalloc.take_snapshot()
-    top_stats = snapshot.statistics('lineno')
+def run_time():
+    '''
+    ru_utime: time in user mode (float)
+    '''
 
-    for stat in top_stats:
-        print(stat)
+    run_time = resource.getrusage(resource.RUSAGE_CHILDREN).ru_utime
+    print('Execution time: ' + str(run_time) + 's')
+
+
+def num_function_calls(file):
+    '''
+    compile(source, filename, mode) -> return a code object
+    The mode argument specifies what kind of code must be compiled:
+    - ‘exec’: source consists of a sequence of statements
+    - ‘eval’: source consists of a single expression
+    - ‘single’: source consists of a single interactive statement
+
+    ex: file: ./smart_db.py
+    -> file[2:]: smart_db.py
+    '''
+
+    compile_target_file = compile(open(file, "r").read(), file, 'exec')
+    pr = cProfile.Profile()
+    pr.enable()
+    pr.run(compile_target_file)
+    pr.disable()
+    ps = pstats.Stats(pr).strip_dirs().sort_stats('calls')
+    ps.print_stats()
 
 
 def main():
-    global target
-    args = handle_wel_args()
-    pr = cProfile.Profile()
-    pr.enable()
-    # pr = cProfile.Profile()
-    # subprocess.run(args.programs, stdout=subprocess.PIPE)
-    subprocess.run(args.programs)
-    pr.disable()
-    pr.print_stats()
-    read_resource(resource.RUSAGE_CHILDREN)
-
-
-def handle_wel_args():
-    parser = ArgumentParser(usage='./benchmarking.py [option]\
- target', description='benchmarking calculate the\
- memory allocation, run-time performance and number of function\
- calls of the program')
-    parser.add_argument('programs', nargs="*")
-    parser.add_argument('-m', '--memory',
-                        help="outputs the memory allocation \
-of the target program",
-                        action="store_true")
-    parser.add_argument('-t', '--time',
-                        help="outputs the execution time \
-(run-time) of the target program",
-                        action="store_true")
-    parser.add_argument('-n', '--number',
-                        help="outputs the number of \
-function calls of the target program",
-                        action="store_true")
-    return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', help='output the memory allocation of the target\
+                        program', action='store_true',)
+    parser.add_argument('-t', help='output the execution time (run-time) of\
+                        the target program', action='store_true')
+    parser.add_argument('-n', help='output the number of function\
+                        calls of the target program',
+                        action='store_true')
+    parser.add_argument('program', help='target program', type=str, nargs='+')
+    args = parser.parse_args()
+    program = args.program
+    subprocess.run(program)
+    if args.m:
+        memory_allocation()
+    if args.t:
+        run_time()
+    if args.n:
+        num_function_calls(program[0])
+    # run with -t if no option is passed
+    if not args.m and not args.t and not args.n:
+        run_time()
 
 
 if __name__ == '__main__':
---
+++ Rush/team_agreement.md
@@ -0,0 +1,22 @@
+# TEAM AGREEMENT
+### -----COFFEEHOLIC-----
+
+### the working hours each member commits to: 9 hours/day
+
+### how often you will check-in with each others' progress: every 3 hours
+
+## the strengths and weaknesses of each member: 
+- Strengths: Quick learners, know how to do research.
+- Weaknesses: Don't have the habits of planning and keeping track of work. 
+
+## the allocation of the workload: 
+- Mission 1: Nam
+- Mission 2: Sang
+- Mission 3: Both of us.
+- The person who finishes his/her mission first will help the other.
+## your strategy so that you all understand the codebase:
+- Do the research thoroughly on the Internet.
+- Read the subject carefully to make sure we understand the requirements.
+- Each person works on their own mission.
+- Then, show the work done to the other and explain everything.
+- Work together for mission 3.
